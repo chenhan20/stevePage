26232252e4d9fd9881905e12091b974286ecfacb
chenhan20 <a0911558945@gmail.com>
2020-04-12 17:24:01 +0800

    Organize projects

---
+++ src/views/LinkTree.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="linkTree">
+    <LinkTree msg="Steve LinkTree"/>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import LinkTree from '@/components/LinkTree.vue'
+
+export default {
+  name: 'linkTree',
+  components: {
+    LinkTree
+  }
+}
+</script>
